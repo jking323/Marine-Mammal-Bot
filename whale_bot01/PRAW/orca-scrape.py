f05516b1bceafc7b6633b21c28c61eb13f7bbf29
jking323 <43685815+jking323@users.noreply.github.com>
2020-08-26 16:29:32 -0400

    Setup PRAW spider to scrape top 500 posts from /r/orcas

--- whale_bot01/PRAW/orca-scrape.py
+++ whale_bot01/PRAW/orca-scrape.py
@@ -1,14 +1,17 @@
+#! python3.8.5
 import logging
 import praw
+import logging.handlers
+import random
+import pandas as pd
+import datetime as dt
+import csv
+import numpy as np
+import os
+
 """
 use praw to pull image URLs then youe scrapy to scrape images from that list
 """
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-for logger_name in ("praw", "prawcore"):
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(Handler)
 
 
 reddit = praw.Reddit(client_id=client_id,
@@ -16,4 +19,17 @@ reddit = praw.Reddit(client_id=client_id,
                      refresh_token=refresh_token,
                      user_agent="orca script by /u/goose323")
 
-print("it works")
+def scrape(sub_scrape):
+    url = []
+    sub_id = []
+    sub = reddit.subreddit(sub_scrape)
+    top_sub = sub.top(limit=500)
+    for submission in top_sub:
+        u = submission.url
+        i = submission.id
+        url.append(u)
+        sub_id.append(i)
+    np.savetxt('C:\Temp\orca.csv', [p for p in zip(url, sub_id)], delimiter=',', fmt ='%s')
+    print("success")
+
+scrape('orcas')
