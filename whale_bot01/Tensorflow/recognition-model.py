88945e1af3ef8b13135d95ae51fdb07ad1d2ff44
jking323 <43685815+jking323@users.noreply.github.com>
2020-08-31 12:31:50 -0400

    Update recognition-model.py
    building out POC learning model

--- whale_bot01/Tensorflow/recognition-model.py
+++ whale_bot01/Tensorflow/recognition-model.py
@@ -1,14 +1,28 @@
-!# python
+#! python
 import itertools
 import os
-
-import matplotlib.pylab as plt
+import PIL
+import PIL.Image
 import numpy as np
-
+import tensorflow_datasets as tfds
 import tensorflow as tf
 import tensorflow_hub as hub
+import pathlib
 
 print("TF version:", tf.__version__)
 print("Hub version:", hub.__version__)
-print("GPU is", "available" if tf.test.is_gpu_available() else "NOT AVAILABLE")
-print("test")
+
+dataset = pathlib.Path("Z:/Workspacescurrent/orca-data/")
+
+batch_size = 32
+img_height = 150
+img_width = 150
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    dataset,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
