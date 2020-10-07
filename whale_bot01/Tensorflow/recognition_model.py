1a59821cf9afe41f835939c9a87e91f8c4553166
jking323 <43685815+jking323@users.noreply.github.com>
2020-10-07 16:36:25 -0400

    commented code
    added comments to recognition model

--- whale_bot01/Tensorflow/recognition_model.py
+++ whale_bot01/Tensorflow/recognition_model.py
@@ -1,3 +1,4 @@
+#import all of the dependencies for the program
 #! python
 import matplotlib.pyplot as plt
 import itertools
@@ -11,17 +12,24 @@ import tensorflow_hub as hub
 from tensorflow.keras import layers
 import pathlib
 import datetime
-#from keras.backend.tensorflow_backend import set_session
+
+#prints the current version of tensorflow
 
 print("TF version:", tf.__version__)
 print("Hub version:", hub.__version__)
 
-dataset = pathlib.Path("E:\git\Marine-Mammal-Bot\orca-data")
-trainset = pathlib.Path("E:\git\Marine-Mammal-Bot\orca-val")
+#defines data and training set directories
+
+dataset = pathlib.Path("C:\whale\orca-data")
+trainset = pathlib.Path("C:\whale\orca-val")
+
+#defines processign batch size and image dimensions
 
 batch_size = 32
 img_height = 150
 img_width = 150
+
+#defines training preprocessing pipeline
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     dataset,
@@ -31,6 +39,8 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_width),
     batch_size=batch_size)
 
+#defines validation image pipeline
+
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     trainset,
     validation_split=0.2,
@@ -39,9 +49,12 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_width),
     batch_size=(batch_size)
 )
+#prints class names to output
 
 class_names = train_ds.class_names
 print(class_names)
+
+#prints image information to output
 
 for image_batch, labels_batch in train_ds:
     print(image_batch.shape)
@@ -56,6 +69,9 @@ for images, labels in train_ds.take(1):
     plt.title(class_names[labels[i]])
     plt.axis("off")
 
+
+#Normalizes data values I believe this sets values to a decimel number between 0 and 1
+
 normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
@@ -66,10 +82,14 @@ print(np.min(first_image), np.max(first_image))
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
+#caches datasets in memory to prevent I/O bottlenecks
+
 train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 num_classes = 5
+
+#defines the model
 
 model = tf.keras.Sequential([
     layers.experimental.preprocessing.Rescaling(1./255),
@@ -84,6 +104,8 @@ model = tf.keras.Sequential([
     layers.Dense(num_classes)
 ])
 
+#compiles the model and allows it to be save for furture use
+
 model.compile(
     optimizer='adam',
     loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -92,32 +114,46 @@ model.compile(
 #log_dir = "E:\git\Logs" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
+#defines training checkpoint paths these checkpoints allow for me to call the model at different points in the 
+#training process and ensures that if an error occurs during one of the training rounds all of the data isn't lost
+
 checkpoint_path = 'E:\git\checkpoints\cp.ckpt'
 checkpoint_dir = os.path.dirname(checkpoint_path)
-
+#defines the checkpoint function
 cp_callbacks = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
-
+#trains the moedl, typically the callbacks would be uncommented to allow checkpints
 model.fit(
     train_ds,
     validation_data=val_ds,
-    epochs=10,
-    callbacks=[cp_callbacks]
+    epochs=1,
+#    callbacks=[cp_callbacks]
 #    callbacks=[tensorboard_callback]
 )
+#Downloads test image to check the model
 
-whale_url = "https://cdn.cnn.com/cnnnext/dam/assets/200728115037-03-orca-whale-pregnant-scn-trnd-exlarge-169.jpg"
+whale_url = "https://images-na.ssl-images-amazon.com/images/I/71h68eptZwL._AC_SX466_.jpg"
 whale_path = tf.keras.utils.get_file('whale', origin=whale_url)
-
+#processes the test image into a format that the model can understand
 img = tf.keras.preprocessing.image.load_img(
     whale_path, target_size=(img_height, img_width)
 )
+#converts test image to array
+
 img_array = tf.keras.preprocessing.image.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)
 
+#uses model to make prediction
+
 predictions = model.predict(img_array)
+
+#quatifies prediction confidence as a %
+
 score = tf.nn.softmax(predictions[0])
+
+#prints prediction and percentage as human readable output
 
 print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
 )
+
